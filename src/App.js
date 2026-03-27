253fde9a73fae88c0b84e1a835b22b798fff315e
Varad Mandhare <varadmandhare924@gmail.com>
2026-03-27 10:09:44 +0530

    Add global state management with useReducer
    Implement global state management using useReducer.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,22 @@
 
+import React, { Fragment, useReducer } from "react";
+import Routes from "./components";
+import { LayoutContext, layoutState, layoutReducer } from "./components/shop";
+
+function App() {
+    // Using useReducer for global state management across the application
+    const [data, dispatch] = useReducer(layoutReducer, layoutState);
+    return ( <
+        Fragment >
+        <
+        LayoutContext.Provider value = {
+            { data, dispatch } } >
+        <
+        Routes / >
+        <
+    );
+}
+
+
+
+export default App;
