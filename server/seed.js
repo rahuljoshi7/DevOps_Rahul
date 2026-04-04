3eaeac1921988a526e86adfcc2851ea08a0830d6
“[varad <varadmandhare924@gmail.com>
2026-04-04 20:51:00 +0530

    fix: remove deprecated useCreateIndex from seed.js

--- server/seed.js
+++ server/seed.js
@@ -7,7 +7,6 @@ const seed = async () => {
   await mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
   });
   console.log("Connected to MongoDB");
 
